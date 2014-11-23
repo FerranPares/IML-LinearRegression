fbb276fe84b4079dca36cdaaab2e762264263487
dgl3 <danigibert24@gmail.com>
2014-11-23 11:43:52 +0100

    Images and plots

--- evaluationModel.m
+++ evaluationModel.m
@@ -2,11 +2,33 @@
 %Question 7 (missing 6)
 load('reg_data_set_2.mat');
 
-%Using firs half for training and second half for validation
+%Using first half for training and second half for validation
 trainx = x(1:(length(x)/2));
 trainy = y(1:(length(y)/2));
+
+figure(1);
+
+scatter(trainx(1),trainy(1),5,'blue');
+hold on;
+for i=2:length(trainx)
+    scatter(trainx(i),trainy(i),5,'blue');
+end
+xlabel('x')
+ylabel('y')
+hold off;
+
 valx = x((length(x)/2+1):length(x));
 valy = y((length(y)/2+1):length(y));
+
+figure(2);
+scatter(valx(1),valy(1),5,'blue');
+hold on;
+for i=2:length(valx)
+    scatter(valx(i),valy(i),5,'blue');
+end
+xlabel('x')
+ylabel('y')
+hold off;
 
 %Computing analitical model
 tx1 = [ones(length(x),1) x ];
@@ -32,33 +54,36 @@ f5 = tw5(1) + tw5(2)*i + tw5(3)*i.^2 + tw5(4)*i.^3 + tw5(5)*i.^4 + tw5(6)*i.^5;
 f6 = tw6(1) + tw6(2)*i + tw6(3)*i.^2 + tw6(4)*i.^3 + tw6(5)*i.^4 + tw6(6)*i.^5 + tw6(7)*i.^6;
 
 %plot validation set and the 6 models
-figure;
+figure(3);
 scatter(valx(1),valy(1),5,'black');
 hold on;
 for p=2:length(valx)
     scatter(valx(p),valy(p),5,'blue');
 end
-plot( i, f1, 'y' );
-plot( i, f2, 'm' );
-plot( i, f3, 'c' );
-plot( i, f4, 'r' );
-plot( i, f5, 'g' );
-plot( i, f6, 'b' );
+hline1 = plot( i, f1, 'y' );
+hline2 = plot( i, f2, 'm' );
+hline3 = plot( i, f3, 'c' );
+hline4 = plot( i, f4, 'r' );
+hline5 = plot( i, f5, 'g' );
+hline6 = plot( i, f6, 'b' );
+legend([hline1,hline2,hline3,hline4,hline5,hline6],'p=1','p=2','p=3','p=4','p=5','p=6','Location','northwest')
+
 hold off;
 
 %plot training set and the 6 models
-figure;
+figure(4);
 scatter(trainx(1),trainy(1),5,'black');
 hold on;
 for p=2:length(trainx)
     scatter(trainx(p),trainy(p),5,'blue');
 end
-plot( i, f1, 'y' );
-plot( i, f2, 'm' );
-plot( i, f3, 'c' );
-plot( i, f4, 'r' );
-plot( i, f5, 'g' );
-plot( i, f6, 'b' );
+hline1 = plot( i, f1, 'y' );
+hline2 = plot( i, f2, 'm' );
+hline3 = plot( i, f3, 'c' );
+hline4 = plot( i, f4, 'r' );
+hline5 = plot( i, f5, 'g' );
+hline6 = plot( i, f6, 'b' );
+legend([hline1,hline2,hline3,hline4,hline5,hline6],'p=1','p=2','p=3','p=4','p=5','p=6','Location','northwest')
 hold off;
 
 %Compute the RMS
@@ -89,8 +114,10 @@ p = 1:6;
 tRMS = [tRMS1 tRMS2 tRMS3 tRMS4 tRMS5 tRMS6];
 vRMS = [vRMS1 vRMS2 vRMS3 vRMS4 vRMS5 vRMS6];
 
-figure;
-plot(p, tRMS, 'g');
+figure(5);
+hline1 = plot(p, tRMS, 'g');
 hold on;
-plot(p, vRMS, 'r');
+hline2 = plot(p, vRMS, 'r');
+legend([hline1,hline2],'tRMS','vRMS','Location','northwest');
+
 hold off;
